6b939920b0d63e71cdead32a820d07b5f9c7eb40
Mingun Pak <mingunpa@usc.edu>
2017-02-24 22:19:53 -0800

    02/24/17 second draft

--- hw6_qkralsrjs/search.php
+++ hw6_qkralsrjs/search.php
@@ -24,6 +24,9 @@
                 Type
                 <select name="searchType">
                     <option value="user">Users</option>
+                    <option value="page">Pages</option>
+                    <option value="event">Events</option>
+                    <option value="group">Groups</option>
                     <option value="place">Places</option>
                 </select>
             </div><br>
@@ -33,42 +36,81 @@
             <input type="reset" name="clear" value="Clear">
         </form>
     </div>
-    <table>
-        <tr>
-            <th>Profile Photo</th>
-            <th>Name</th>
-            <th>Details</th>
-        </tr>
-    <!-- facebook API-->
-    <?php
-        if ($_SERVER["REQUEST_METHOD"] == POST){
-            $q = "q=";
-            $type = "type=";
-            $fields = "fields=id,name,picture.width(700).height(700)";
-            $access_token = "access_token=EAACEdEose0cBAKMvMXFOL5b522rZB2Hmk6xZBr4yLslGOlZBktsZANvZAT6WJBid22XacelhmW7dnXj0ieTAm2bBeZBDMYBO8SGjAtqqsow1vaR4DKQEMVWYEm1p6YyNxhzynjq9JhjdwBx1K6XRbZCkXRyNTnt8NMkKyd7G8tKZBMrKYLwzIZBNpBCo5oLVC47gZD";
-            $url= "https://graph.facebook.com/v2.8/search?";
-            $url .= $q . $_POST["keyword"] . "&" . $type . $_POST[searchType] . "&" . $fields . "&" . $access_token;
+    <div class="bottom">
 
-            $response = file_get_contents($url);
-            $jsonData = json_decode($response);
-            //echo $jsonData -> data[0] -> id;
-            //echo $jsonData -> data[0] -> name;
-            //echo $jsonData -> data[0] -> picture -> width;
-            //echo $jsonData -> data[0] -> picture -> height;
-            //echo $jsonData -> data[0] -> picture -> is_silhouette;
-            //echo $jsonData -> data[0] -> picture -> url;
-            foreach($jsonData -> data as $each){
-                echo "<tr>";
-                echo "<td>" . "<img src='" . $each -> picture -> data -> url . "' height=30px width=40px>" . "</td>";
-                echo "<td>" . $each -> name . "</td>";
-                echo "<td>" . "<a href='https://facebook.com'>Details</a>" . "</td>";
-                echo "</tr>";
+        <!-- facebook API: post request-->
+        <?php
+            if ($_SERVER["REQUEST_METHOD"] == POST){
+                echo "<table><tr><th>Profile Photo</th><th>Name</th><th>Details</th></tr>";
+                $q = "q=";
+                $type = "type=";
+                $fields = "fields=id,name,picture.width(700).height(700)";
+                $access_token = "access_token=EAACEdEose0cBAEPs9szR6zNdktY414kSvo6itIAYqY2QChuNELGBDPpm9ruRqcEtE6w44itJPhtgpX12w2GZCiZC9DiJKlDSPVqRZC9EZCwTK3jp9Fy0amPzhEkdBlCZCLTHYsdvptZCi9EHIEj8vg3ErjwGv4HtfiJZAimaejVDLQKVE11Miq6g4ECZBqAioucZD";
+                $url= "https://graph.facebook.com/v2.8/search?";
+
+                if($_POST[searchType] == "place"){
+
+                    $google_access_token = "AIzaSyAT1QKwxisgM8fQypxReHmnxJ2gPTPdB90";
+                    $address = $_POST['location'];
+                    $google_url = "https://maps.googleapis.com/maps/api/geocode/json?address=". $address . "&key=" . $google_access_token;
+                    $google_url = str_replace(' ','%20', $google_url);
+                    $google_response = file_get_contents($google_url);
+                    $google_json_data = json_decode($google_response);
+                    $lat = $google_json_data -> results[0] -> geometry -> location -> lat;
+                    $lng = $google_json_data -> results[0] -> geometry -> location -> lng;
+
+                    $center = "center=" . $lat ."," . $lng;
+                    $distance = "distance=" . $_POST["distance"];
+                    $url .= $q . $_POST["keyword"] . "&" . $type . $_POST[searchType] . "&" . $center . "&" . $distance . "&" . $fields . "&" . $access_token;
+                }
+                else{
+                    $url .= $q . $_POST["keyword"] . "&" . $type . $_POST[searchType] . "&" . $fields . "&" . $access_token;
+                }
+
+                $url = str_replace(' ','%20', $url);
+                $response = file_get_contents($url);
+                $jsonData = json_decode($response);
+                foreach($jsonData -> data as $each){
+                    echo "<tr>";
+                    echo "<td>" . "<a href='" . $each -> picture -> data -> url . "'><img src='" . $each -> picture -> data -> url . "' height=30px width=40px></a>" . "</td>";
+                    echo "<td>" . $each -> name . "</td>";
+                    echo "<td>" . "<a href='" . htmlspecialchars($_SERVER["PHP_SELF"]) . "?id=" . $each -> id . "'>Details</a>" . "</td>";
+                    echo "</tr>";
+                }
+                echo "</table>";
             }
-        }
-    ?>
-    </table>
+        ?>
 
-    <div class="bottom">result</div>
+        <!-- facebook API: click detail-->
+        <?php
+            if (isset($_GET['id'])){
+                echo "<table><tr><th>Albums</th></tr>";
+                $fields = "?fields=id,name,picture.width(700).height(700),albums.limit(5){name,photos.limit(2) {name, picture}},posts.limit(5)";
+                $access_token = "access_token=EAACEdEose0cBAEPs9szR6zNdktY414kSvo6itIAYqY2QChuNELGBDPpm9ruRqcEtE6w44itJPhtgpX12w2GZCiZC9DiJKlDSPVqRZC9EZCwTK3jp9Fy0amPzhEkdBlCZCLTHYsdvptZCi9EHIEj8vg3ErjwGv4HtfiJZAimaejVDLQKVE11Miq6g4ECZBqAioucZD";
+                $url= "https://graph.facebook.com/v2.8/" . $_GET['id'] . $fields . "&" . $access_token;
+                $url = str_replace(' ','%20', $url);
+                $response = file_get_contents($url);
+                $jsonData = json_decode($response);
+                if (isset($jsonData -> albums -> data)){
+                    foreach($jsonData -> albums -> data as $each){
+                        echo "<tr><td>" . $each -> name . "</td></tr>";
+                        echo "<tr>";
+                        foreach($each -> photos -> data as $data){
+                            echo "<td>" . "<a href='" . $data -> picture . "'><img src='" . $data -> picture . "' height=80px width=80px></a>" . "</td>";
+                        }
+                        echo "</tr>";
+                    }
+                }
+                echo "<table><tr><th>Message</th></tr>";
+                if (isset($jsonData -> posts -> data)){
+                    foreach($jsonData -> posts -> data as $each){
+                        echo "<tr><td>" . $each -> message . "</td></tr>";
+                    }
+                }
+                echo "</table>";
+            }
+        ?>
+    </div>
 
 </body>
 </html>
